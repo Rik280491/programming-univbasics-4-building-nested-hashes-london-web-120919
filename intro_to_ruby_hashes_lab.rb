38d8cad69ee5148e04eb007d7419852c5c44b118
Rikesh Balakrishnan <rik280491@googlemail.com>
2019-11-13 20:06:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ def monopoly_with_third_tier
     
     :names => {
       :reading_railroad => { }
-      :pennsylvania_railroad => {"mortgage_value" => "$200"}
+      :pennsylvania_railroad => {}
       :b_and_o_railroad => {}
       :shortline_railroad => {"mortgage_value" => "$800"} 
       }
